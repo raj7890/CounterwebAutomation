29f09678861c734c6757650422c2f6d2f6d0e35b
raj7890 <rkmr7890@gmail.com>
2017-10-23 22:09:15 +0530

    Update AppTest.java
    asdf

--- src/test/java/CounterWebApp/CounterWebApp/AppTest.java
+++ src/test/java/CounterWebApp/CounterWebApp/AppTest.java
@@ -28,7 +28,7 @@ public class AppTest
 		WebDriver driver = new ChromeDriver();
 		//WebDriver driver  = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("http://172.16.3.61:8080/HRMSDP/");
+		driver.get("http://localhost:8080/HRMSDP/");
 		
 		String eletext = driver.findElement(By.xpath("//h2[contains(.,'Please sign in')]")).getText();
 	
@@ -38,63 +38,63 @@ public class AppTest
 		
 	}
 	
-//	@Test
-//	public void verifylogin()
-//	{
-//		WebDriver driver  = new FirefoxDriver();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("http://172.16.3.61:8080/HRMSDP");
-//		String eletext = driver.findElement(By.xpath("//h2[contains(.,'Please sign in')]")).getText();
-//		Assert.assertEquals(eletext, "Please sign in");
-//		//driver.close();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.findElement(By.xpath("//input[@name='user.username']")).sendKeys("admin");
-//		driver.findElement(By.xpath("//input[@name='user.password']")).sendKeys("12345");
-//		driver.findElement(By.xpath("//input[@type='submit']")).click();	
-//		String StrMasters= driver.findElement(By.xpath("//a[contains(.,'Masters')]")).getText();
-//		Assert.assertEquals(StrMasters, "Masters");
-//		WebElement ele = driver.findElement(By.xpath("//a[@class='dropdown-toggle']"));
-//		Actions action = new Actions(driver);
-//		action.moveToElement(ele).build().perform();
-//		driver.findElement(By.xpath("//a[contains(.,'Logout')]")).click();
-//		String Strusername = driver.findElement(By.xpath("//input[@name='user.username']")).getAttribute("placeholder");
-//		Assert.assertEquals(Strusername, "Username");
-//		
-//	
-//	}
-//	
-//	
-//	@Test
-//	public void verifyMasterPage()
-//	{
-//		WebDriver driver  = new FirefoxDriver();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("http://172.16.3.61:8080/HRMSDP");
-//		String eletext = driver.findElement(By.xpath("//h2[contains(.,'Please sign in')]")).getText();
-//		Assert.assertEquals(eletext, "Please sign in");
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.findElement(By.xpath("//input[@name='user.username']")).sendKeys("admin");
-//		driver.findElement(By.xpath("//input[@name='user.password']")).sendKeys("12345");
-//		driver.findElement(By.xpath("//input[@type='submit']")).click();	
-//		String StrMasters= driver.findElement(By.xpath("//a[contains(.,'Masters')]")).getText();
-//		Assert.assertEquals(StrMasters, "Masters");
-//		driver.findElement(By.xpath("//a[contains(.,'Masters')]")).click();	
-//		WebElement mySelectElement = driver.findElement(By.xpath("//select[@class='dropdown dropbtn ']"));
-//		Select dropdown= new Select(mySelectElement);
-//		dropdown.selectByValue("EmpTypeDisplay");
-//		driver.findElement(By.xpath("//input[@value='Go']")).click();
-//		driver.findElement(By.xpath("//input[@value='Add Employee Type']")).click();
-//		driver.findElement(By.xpath("//input[@name='empf.empTypeId']")).sendKeys("R&D");
-//		driver.findElement(By.xpath("//input[@name='empf.description']")).sendKeys("Reasearch & Development");
-//		WebElement ele = driver.findElement(By.xpath("//a[@class='dropdown-toggle']"));
-//		Actions action = new Actions(driver);
-//		action.moveToElement(ele).build().perform();
-//		driver.findElement(By.xpath("//a[contains(.,'Logout')]")).click();
-//		String Strusername = driver.findElement(By.xpath("//input[@name='user.username']")).getAttribute("placeholder");
-//		Assert.assertEquals(Strusername, "Username");
-//		
-//	
-//	}
+	@Test
+	public void verifylogin()
+	{
+		WebDriver driver  = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost:8080/HRMSDP");
+		String eletext = driver.findElement(By.xpath("//h2[contains(.,'Please sign in')]")).getText();
+		Assert.assertEquals(eletext, "Please sign in");
+		//driver.close();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@name='user.username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='user.password']")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();	
+		String StrMasters= driver.findElement(By.xpath("//a[contains(.,'Masters')]")).getText();
+	Assert.assertEquals(StrMasters, "Masters");
+		WebElement ele = driver.findElement(By.xpath("//a[@class='dropdown-toggle']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(ele).build().perform();
+		driver.findElement(By.xpath("//a[contains(.,'Logout')]")).click();
+		String Strusername = driver.findElement(By.xpath("//input[@name='user.username']")).getAttribute("placeholder");
+		Assert.assertEquals(Strusername, "Username");
+		
+	
+	}
+	
+	
+	@Test
+	public void verifyMasterPage()
+	{
+		WebDriver driver  = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost:8080/HRMSDP");
+		String eletext = driver.findElement(By.xpath("//h2[contains(.,'Please sign in')]")).getText();
+		Assert.assertEquals(eletext, "Please sign in");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@name='user.username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='user.password']")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();	
+		String StrMasters= driver.findElement(By.xpath("//a[contains(.,'Masters')]")).getText();
+		Assert.assertEquals(StrMasters, "Masters");
+		driver.findElement(By.xpath("//a[contains(.,'Masters')]")).click();	
+		WebElement mySelectElement = driver.findElement(By.xpath("//select[@class='dropdown dropbtn ']"));
+		Select dropdown= new Select(mySelectElement);
+		dropdown.selectByValue("EmpTypeDisplay");
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		driver.findElement(By.xpath("//input[@value='Add Employee Type']")).click();
+		driver.findElement(By.xpath("//input[@name='empf.empTypeId']")).sendKeys("R&D");
+		driver.findElement(By.xpath("//input[@name='empf.description']")).sendKeys("Reasearch & Development");
+		WebElement ele = driver.findElement(By.xpath("//a[@class='dropdown-toggle']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(ele).build().perform();
+		driver.findElement(By.xpath("//a[contains(.,'Logout')]")).click();
+		String Strusername = driver.findElement(By.xpath("//input[@name='user.username']")).getAttribute("placeholder");
+		Assert.assertEquals(Strusername, "Username");
+		
+	
+	}
 	
 	
 	
